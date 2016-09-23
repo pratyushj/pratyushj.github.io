3897c87543edb78644c3888502e964d306322129
Pratyush <pratyush.jha28@gmail.com>
2016-09-23 09:58:40 +0530

    Adding a POC for session storage with document context testing across browsers

---
+++ sessionStoragePOC/app.js
@@ -0,0 +1,14 @@
+window.onload =  function(){
+	var itemName = 'item';
+	var count = sessionStorage.getItem(itemName) || 0;
+
+	count++;
+	document.querySelector('#counter').innerHTML =  count;
+	sessionStorage.setItem(itemName, count);
+	document.querySelector('#newWindowID').onclick  = function(){
+		window.open('/app.html') ;
+		e.stopPropagation();
+		return false;
+	}
+
+}
